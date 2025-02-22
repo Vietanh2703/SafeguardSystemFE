ca0d9110868c5bc6d9622c3742f4daeaa7a409f6
Thinhlubbin <chinsucuc@gmail.com>
2025-02-22 15:49:58 +0700

    UI_loginPagewithGoogle

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAc-g3kLguUfx8X2zOgNHF5onE_QcOoaf0",
+    authDomain: "heso-hrm.firebaseapp.com",
+    projectId: "heso-hrm",
+    storageBucket: "heso-hrm.firebasestorage.app",
+    messagingSenderId: "243212199762",
+    appId: "1:243212199762:web:a5ddddcd9236e525547c8b",
+    measurementId: "G-HGJZP0NJ77",
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+export const auth = getAuth();
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AdminDashboard from './pages/dashboard/index.jsx';
+import LoginPage from './pages/login/index.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+
+
+  {
+    path: "/dashboard",
+    element: <AdminDashboard/>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
+ 
 )
