bd7a4cec30589ec852d05dfb8533f508c3cbbd38
Thinhlubbin <chinsucuc@gmail.com>
2025-03-03 19:13:54 +0700

    viewroleviewrole

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,11 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AdminDashboard from './pages/dashboard/index.jsx';
 import LoginPage from './pages/login/index.jsx';
-<<<<<<< Updated upstream
 import MaintenancePage from './pages/maintenance/index.jsx';
-=======
-import SecurityGuard from './pages/securityguard/index.jsx';
 import BusinessPartner from './pages/businesspartner/index.jsx';
 import SecurityManager from './pages/securitymanager/index.jsx';
 import ViewRole from './pages/viewRole/index.jsx';
 
->>>>>>> Stashed changes
 
 const router = createBrowserRouter([
   {
@@ -30,8 +26,6 @@ const router = createBrowserRouter([
     path: "/maintenance",
     element: <MaintenancePage/>,
   },
-<<<<<<< Updated upstream
-=======
 
   {
     path: "/businesspartner",
@@ -48,10 +42,7 @@ const router = createBrowserRouter([
     element: <ViewRole/>,
   },
 
-
->>>>>>> Stashed changes
 ]);
-
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
  
